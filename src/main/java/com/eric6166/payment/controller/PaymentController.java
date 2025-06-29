8150e7df38c5eea8929d30a70071126ceb32613a
eric6166 <ericnguyen6166@gmail.com>
2025-06-29 19:04:22 +0700

    commit

--- src/main/java/com/eric6166/payment/controller/PaymentController.java
+++ src/main/java/com/eric6166/payment/controller/PaymentController.java
@@ -1,11 +1,9 @@
 package com.eric6166.payment.controller;
 
-import com.eric6166.base.utils.BaseMessageConst;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +21,6 @@ import java.util.Map;
 public class PaymentController {
 
     private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
-    private final MessageSource messageSource;
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
@@ -35,9 +32,6 @@ public class PaymentController {
     public ResponseEntity<Map<String, String>> test() {
         log.info("test");
         Map<String, String> map = new HashMap<>();
-        map.put(BaseMessageConst.MSG_INF_RESOURCE_CREATED, messageSource.getMessage(BaseMessageConst.MSG_INF_RESOURCE_CREATED, new String[]{"abc"}, LocaleContextHolder.getLocale()));
-        map.put(BaseMessageConst.MSG_INF_RESOURCE_UPDATED, messageSource.getMessage(BaseMessageConst.MSG_INF_RESOURCE_UPDATED, new String[]{"abc"}, LocaleContextHolder.getLocale()));
-        map.put(BaseMessageConst.MSG_ERR_RESOURCE_EXISTED, messageSource.getMessage(BaseMessageConst.MSG_ERR_RESOURCE_EXISTED, new String[]{"abc"}, LocaleContextHolder.getLocale()));
         map.put(ZoneOffset.UTC.getId(), LocalDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MICROS).toString());
         return ResponseEntity.ok(map);
     }
